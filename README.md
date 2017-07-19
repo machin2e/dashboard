cadb0b34ad862def5b55cb558a6f7318bf9698f4
Michael Gubbels <mrgubbels@gmail.com>
2017-07-18 22:15:18 -0700

    Added GitHub login flow and authenticated call.

--- README.md
+++ README.md
@@ -2,9 +2,29 @@
 
 The dashboard manages user workspaces.
 
+# Setup
+
+Install `npm` dependencies:
+
+```
+npm update
+```
+
+Place GitHub authentication credentials (the client ID and client secret) into files named `GITHUB_CLIENT_ID` and `GITHUB_CLIENT_SECRET`, respectively.
+
+# Usage
+
+Start the server:
+
+```
+npm start
+```
+
 # Tech Stack
 
+- Docker
 - Node, Express, Passport
+- Bootstrap
 - Redis for storing user account data and sessions
 
 # Resources
--- app.js
+++ app.js
@@ -7,9 +7,11 @@ var methodOverride = require('method-override');
 var GitHubStrategy = require('passport-github2').Strategy;
 var partials = require('express-partials');
 
+const https = require('https');
+const request = require('request');
 
-var GITHUB_CLIENT_ID = "" // "--insert-github-client-id-here--";
-var GITHUB_CLIENT_SECRET = "" // "--insert-github-client-secret-here--";
+var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID // "--insert-github-client-id-here--";
+var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET // "--insert-github-client-secret-here--";
 
 
 // Passport session setup.
@@ -20,11 +22,11 @@ var GITHUB_CLIENT_SECRET = "" // "--insert-github-client-secret-here--";
 //   have a database of user records, the complete GitHub profile is serialized
 //   and deserialized.
 passport.serializeUser(function(user, done) {
-  done(null, user);
+	done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+	done(null, obj);
 });
 
 
@@ -33,23 +35,32 @@ passport.deserializeUser(function(obj, done) {
 //   credentials (in this case, an accessToken, refreshToken, and GitHub
 //   profile), and invoke a callback with a user object.
 passport.use(new GitHubStrategy({
-    clientID: GITHUB_CLIENT_ID,
-    clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-	// TODO: Look up user in Machineee database by ID or authenticated token.
-	// TODO: Load profile from Machineee database and return it instead of the Passport profile.
-    process.nextTick(function () {
-      
-      // To keep the example simple, the user's GitHub profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the GitHub account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
-  }
+		clientID: GITHUB_CLIENT_ID,
+		clientSecret: GITHUB_CLIENT_SECRET,
+		callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+	},
+	function(accessToken, refreshToken, profile, done) {
+		// asynchronous verification, for effect...
+		// TODO: Look up user in Machineee database by ID or authenticated token.
+		// TODO: Load profile from Machineee database and return it instead of the Passport profile.
+
+
+		// TODO: Replace this with the tokens stored in the user's DB record.
+		profile.oauth = {
+			accessToken: accessToken,
+			refreshToken: refreshToken
+		};
+
+
+		process.nextTick(function() {
+
+			// To keep the example simple, the user's GitHub profile is returned to
+			// represent the logged-in user.  In a typical application, you would want
+			// to associate the GitHub account with a user record in your database,
+			// and return that user instead.
+			return done(null, profile);
+		});
+	}
 ));
 
 
@@ -61,10 +72,16 @@ var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
 app.use(methodOverride());
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: false
+}));
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 app.use(passport.initialize());
@@ -72,21 +89,76 @@ app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	console.log(req.user);
-  res.render('index', { user: req.user });
+	res.render('index', {
+		user: req.user
+	});
 });
 
 app.get('/projects', ensureAuthenticated, function(req, res) {
-	res.render('projects', { user: req.user });
+
+
+
+	// TODO: Move this into the /projects request handler.
+	request.get({
+		// url: 'https://api.github.com/users/mgub/repos', 
+		url: 'https://api.github.com/user/repos',
+		headers: {
+			'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+			'Authorization': 'token ' + req.user.oauth.accessToken
+		},
+		//,
+		//oauth: {
+		////callback: 'http://127.0.0.1:3000/auth/github/callback',
+		//consumer_key: GITHUB_CLIENT_ID,
+		//consumer_secret: GITHUB_CLIENT_SECRET,
+		//token: accessToken,
+		//token_secret: refreshToken,
+		//verifier: profile.provider
+		//},
+		json: true
+	}, function(err, github_response, body) {
+		if (!err && github_response.statusCode == 200) {
+
+			// Body is already parsed since request optoin 'json:true' was used.
+			var repos = [];
+			body.forEach(function(repo) {
+				repos.push({
+					name: repo.name,
+					description: repo.description
+				});
+			});
+			console.log('the repos are  ' + JSON.stringify(repos));
+			req.user.repos = repos;
+
+
+
+
+			// Pipe to view rendering function.
+			// TODO: Pipe this in a better way?
+			res.render('projects', {
+				user: req.user
+			});
+		}
+	});
+
+
+	//res.render('projects', {
+	//user: req.user
+	//});
 });
 
-app.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
+app.get('/account', ensureAuthenticated, function(req, res) {
+	res.render('account', {
+		user: req.user
+	});
 });
 
-app.get('/login', function(req, res){
-  res.render('login', { user: req.user });
+app.get('/login', function(req, res) {
+	res.render('login', {
+		user: req.user
+	});
 });
 
 // GET /auth/github
@@ -95,28 +167,34 @@ app.get('/login', function(req, res){
 //   the user to github.com.  After authorization, GitHub will redirect the user
 //   back to this application at /auth/github/callback
 app.get('/auth/github',
-  passport.authenticate('github', { scope: [ 'user:email' ] }),
-  function(req, res){
-    // The request will be redirected to GitHub for authentication, so this
-    // function will not be called.
-  });
+	passport.authenticate('github', {
+		scope: ['user:email']
+	}),
+	function(req, res) {
+		// The request will be redirected to GitHub for authentication, so this
+		// function will not be called.
+	});
 
 // GET /auth/github/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+app.get('/auth/github/callback',
+	passport.authenticate('github', {
+		failureRedirect: '/login'
+	}),
+	function(req, res) {
+		res.redirect('/');
+	});
 
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
 });
 
+
+console.log('Starting to listen.');
 app.listen(3000);
 
 
@@ -126,6 +204,8 @@ app.listen(3000);
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login')
 }
